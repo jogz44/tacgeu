f78ebc76733a6b09e876512f96e47a6b437ae336
jogz44 <annjogz@gmail.com>
2026-07-17 13:19:26 +0800

    edited images part 3

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -3,9 +3,9 @@ import { ImgHTMLAttributes } from 'react';
 export default function AppLogoIcon(props: ImgHTMLAttributes<HTMLImageElement>) {
     return (
         <img
-            {...props}
             src={`${window.APP_BASE_URL}assets/icon/app-logo.svg`}
             alt="Tagum City Government Employees' Union"
+            {...props}
         />
     );
 }
